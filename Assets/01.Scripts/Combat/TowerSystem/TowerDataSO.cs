80f08c4d842258052fcc33298bf68b632665a643
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-08-23 21:06:20 +0900

    feat: Tower Struct

---
+++ Assets/01.Scripts/Combat/TowerSystem/TowerDataSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+namespace Project_Train.Combat.TowerSystem
+{
+
+    public class TowerDataSO : MonoBehaviour
+    {
+        public string towerName;
+        public string towerDescription;
+        public Tower towerPrefab;
+
+        
+        
+    }
+}
---
+++ Assets/01.Scripts/Combat/TowerSystem/TowerPart/Tower.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+namespace Project_Train.Combat.TowerSystem
+
+{
+    public class Tower : MonoBehaviour
+    {
+        [SerializeField] private TowerBase _base;
+        
+        //
+    }
+}
