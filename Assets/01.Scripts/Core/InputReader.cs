462ee434a9a7c3e4c0297f7e0ef7747c22130b92
Crogen0407 <mymcoding0407@gmail.com>
2025-08-24 23:09:11 +0900

    feat : InputReader

---
+++ Assets/01.Scripts/Core/InputReader.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace  Project_Train.Core
+{
+	public static class InputReader
+	{
+		public static Controls Controls { get; private set; }
+		public static Controls.PlayerActions PlayerActions { get; private set; }
+		public static Controls.UIActions UIActions { get; private set; }
+
+		#region Actions
+
+		// 이벤트는 여기에 작성
+
+		#endregion
+
+		#region Values
+
+		// 프로퍼티는 여기에 작성
+
+		#endregion
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void Initialize()
+		{
+			if (Controls != null) return;
+
+			Controls = new Controls();
+			PlayerActions = Controls.Player;
+			UIActions = Controls.UI;
+
+			PlayerActions.SetCallbacks(new PlayerCallback());
+			UIActions.SetCallbacks(new UICallback());
+
+			Controls.Enable();
+		}
+
+		#region Callbacks
+
+		private class PlayerCallback : Controls.IPlayerActions
+		{
+			// 인터페이스 구현은 여기에
+			public void OnMove(InputAction.CallbackContext context)
+			{
+			}
+		}
+
+		private class UICallback : Controls.IUIActions
+		{
+			public void OnCancel(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnClick(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnMiddleClick(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnNavigate(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnPoint(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnRightClick(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnScrollWheel(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnSubmit(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnTrackedDeviceOrientation(InputAction.CallbackContext context)
+			{
+			}
+
+			public void OnTrackedDevicePosition(InputAction.CallbackContext context)
+			{
+			}
+		}
+
+		#endregion
+	}
+}
