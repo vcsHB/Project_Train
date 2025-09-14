d228bff2c1944f4a90e4b98330e8bef03a3a8109
vcs <99899703+vcsHB@users.noreply.github.com>
2025-09-14 22:43:42 +0900

    feat: Camera Lock Panel

---
+++ Assets/01.Scripts/BuildSystem/SubObjects/BuildFrameStatusPanel.cs
@@ -0,0 +1,10 @@
+using Project_Train.UIManage.WorldCanvas;
+using UnityEngine;
+namespace Project_Train.BuildSystem.SubObjects
+{
+
+    public class BuildFrameStatusPanel : CameraLockPanel
+    {
+        
+    }
+}
