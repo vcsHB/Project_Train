3158d9f414c2cfd1afefe9ca4a342d50a8474c87
Crogen0407 <mymcoding0407@gmail.com>
2025-09-20 03:16:12 +0900

    feat : Wave Struction

---
+++ Assets/01.Scripts/Combat/TrainSystem/TrainArraySO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace  Project_Train
+{
+    [CreateAssetMenu(fileName = "TrainArraySO", menuName = "Scriptable Objects/TrainArraySO")]
+    public class TrainArraySO : ScriptableObject
+    {
+    
+    }
+}
---
+++ Assets/01.Scripts/Combat/WaveSystem/WaveSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Project_Train.Combat.WaveSystem
+{
+    [System.Serializable]
+    public struct WaveTrainData
+    {
+        public float startDelay;
+        public TrainArraySO trainArraySO;
+	}
+
+	[CreateAssetMenu(fileName = "WaveSO", menuName = "SO/WaveSO")]
+    public class WaveSO : ScriptableObject
+    {
+        [field: SerializeField] public WaveTrainData[] waveTrains;
+	}
+}
